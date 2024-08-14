2c9efca1389dbc19fbb0f5aff882a417346636af
Milind Sarkar <milindsarkar018@gmail.com>
2024-08-14 12:35:57 +0530

    Update install_r2u.R

--- install_r2u.R
+++ install_r2u.R
@@ -9,22 +9,31 @@ if (!dir.exists(custom_lib_path)) {
 # Set the library path
 .libPaths(custom_lib_path)
 
-# List of packages to install
-packages <- c(
-    "ggplot2", "gplots", "SuperLearner", "foreach", "parallel", "doParallel", "unbalanced",
-    "plyr", "party", "ROCR", "pROC", "rbind", "cbind", "abind", "caret", "glmnet", "e1071",
-    "GGally"
-    #"SuperLearner"
+# Get the list of all installed packages in the default library
+installed_packages <- installed.packages(lib.loc = .libPaths()[2])
+
+# List of package names from the default library
+default_packages <- installed_packages[, "Package"]
+
+# List of additional packages to install
+new_packages <- c(
+    #"ggplot2", "gplots", "SuperLearner", "foreach", "parallel", "doParallel", "unbalanced",
+    #"plyr", "party", "ROCR", "pROC", "rbind", "cbind", "abind", "caret", "glmnet", "e1071",
+    #"GGally"
+    "SuperLearner"
 )
 
-# Install packages if not already installed
+# Combine all packages (default + new)
+packages <- unique(c(default_packages, new_packages))
+
+# Install and load packages in the custom library path if not already present
 for (pkg in packages) {
-    if (!require(pkg, character.only = TRUE)) {
+    if (!require(pkg, character.only = TRUE, lib.loc = custom_lib_path)) {
         install.packages(pkg, lib = custom_lib_path)
-        library(pkg, character.only = TRUE)
-        cat(paste("Installed and loaded package:", pkg, "\n"))
+        library(pkg, character.only = TRUE, lib.loc = custom_lib_path)
+        cat(paste("Installed and loaded package in custom library:", pkg, "\n"))
     } else {
-        cat(paste("Package already installed and loaded:", pkg, "\n"))
+        cat(paste("Package already present in custom library and loaded:", pkg, "\n"))
     }
 }
 
