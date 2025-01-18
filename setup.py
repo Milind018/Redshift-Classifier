3d64a5dfbd156b19a73748e8a0676c8a1a340586
Milind Sarkar <milindsarkar018@gmail.com>
2025-01-18 18:11:58 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="ggt",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Redshift-Classifier(X-Ray)",
+    author="Shubham Bhardwaj, Milind Sarkar",
+    license="GNU GPL v3",
+)
